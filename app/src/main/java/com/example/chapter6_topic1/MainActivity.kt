46b456b25ee1cb84afbe9678561729ace3852272
Raynaldi <raynaldizoel@gmail.com>
2022-10-11 20:57:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chapter6_topic1/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.chapter6_topic1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.chapter6_topic1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    private val viewModel : BlurViewModel by viewModels {BlurViewModel.BlurViewModelFactory(application)  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goButton.setOnClickListener{
+            viewModel.applyBlur(blurLevel)
+        }
+    }
+
+    private val blurLevel: Int
+        get() =
+            when (binding.radioBlurGroup.checkedRadioButtonId) {
+                R.id.radio_blur_lv_1 -> 1
+                R.id.radio_blur_lv_2 -> 2
+                R.id.radio_blur_lv_3 -> 3
+                else -> 1
+            }
+}
